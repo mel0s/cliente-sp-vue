25679751c89ddfa6c411653019ccae1c105000fc
Oswaldo Sanchez Melos <programador.o@aige.com.mx>
2021-09-28 19:20:12 -0500

    Agregando archivo de configuracion

--- init.js
+++ init.js
@@ -1,15 +1,33 @@
 let ws = "wss://socket.svanesa.online";
-let token = "";
-let id = "TEST_SVANESA";
-let tokenApi = "";
-let llave = "";
-let sistemaOrigenId = "";
+let token = "token-prueba";
+let tokenApi = "token-svanesa";
+let llave = "L1234567890";
+let sistemaOrigenId = "s123456780";
+import init from "../../sp.config.js";
+
+let v = init.variables;
+
+if (v) {
+  if (v.llave)
+    llave = v.llave;
+
+  if (v.token)
+    token = v.token;
+
+  if (v.tokenApi)
+    tokenApi = v.tokenApi;
+
+  if (v.sistemaOrigenId)
+    tokenApi = v.sistemaOrigenId;
+
+
+}
+
 
 export default {
   ws,
   token,
   llave,
   tokenApi,
-  sistemaOrigenId,
-  id
+  sistemaOrigenId
 }
